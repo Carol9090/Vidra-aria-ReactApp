52f825e67b4225393aabaf3bc46d523b51aeaa0c
Carol9090 <111928630+Carol9090@users.noreply.github.com>
2022-08-23 21:45:10 -0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Img from './components/logo';
+
+
+function App(){
+  return (
+    <div >
+      
+      
+      
+      <Img/>
+    </div>
+  
+  );
+}; 
+
+export default App;
---
+++ frontend/src/components/logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Area from '../img/area.jpg'
+import Escada from '../img/escada.png'
+import Espelhos from '../img/espelhos.jpg'
+import Fundo from '../img/Fundo.jpg'
+import Lago from '../img/LOGO.png'
+
+function Img() {
+
+    return (
+  
+      <div>
+    
+            <img src={Area }/>
+            <img src={Fundo }/>
+            <img src={Escada}/>
+            <img src={Lago}/>
+            <img src={Espelhos}/>
+            <img src={Espelhos}/>
+      </div>
+    
+    );
+  }
+export default Img;
